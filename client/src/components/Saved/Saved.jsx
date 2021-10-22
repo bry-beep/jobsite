6b581893b1a1f7596c0c808279002c77f00bd277
Bryanna Valdivia <vbryanna94@gmail.com>
2021-10-22 12:23:02 -0700

    initial

--- client/src/components/Saved/Saved.jsx
+++ client/src/components/Saved/Saved.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-expressions */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 
@@ -13,21 +15,9 @@ const Saved = ({ user }) => {
       .then((response) => {
         setSaved(response.data);
       })
-      .catch((error) => {
-        console.log('Error getting saved items: ', error);
+      .catch(() => {
+        throw new Error('Error getting saved items: ');
       });
-  };
-
-  const removeSaved = (savedId) => {
-    console.log(savedId);
-    // axios
-    //   .delete(`http://localhost:4008/saved/${savedId}`)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error deleting saved item: ', error);
-    //   });
   };
 
   useEffect(() => {
@@ -50,25 +40,28 @@ const Saved = ({ user }) => {
         {user !== null && (
           <div
             className="card"
-            style={{ backgroundColor: '#ACC196', color: 'white', padding: '10px 20px 10px 20px', border: '1px solid #49475B' }}
+            style={{
+              backgroundColor: '#ACC196',
+              color: 'white',
+              padding: '10px 20px 10px 20px',
+              border: '1px solid #49475B',
+            }}
           >
             <h3>Interested</h3>
             {saved
               .filter((item) => item.level === 'interested')
-              .map((savedItem) => {
-                return (
-                  <div key={savedItem._id} className="card">
-                    {item}
-                    <button
-                      type="button"
-                      onClick={() => removeSaved(savedItem._id)}
-                      className="mainButton"
-                    >
-                      X
-                    </button>
-                  </div>
-                );
-              })}
+              .map((savedItem) => (
+                <div key={savedItem._id} className="card">
+                  {item}
+                  <button
+                    type="button"
+                    onClick={() => removeSaved(savedItem._id)}
+                    className="mainButton"
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
           </div>
         )}
       </div>
@@ -76,7 +69,12 @@ const Saved = ({ user }) => {
         {user !== null && (
           <div
             className="card"
-            style={{ backgroundColor: '#799496', color: 'white', padding: '10px 20px 10px 20px', border: '1px solid #49475B' }}
+            style={{
+              backgroundColor: '#799496',
+              color: 'white',
+              padding: '10px 20px 10px 20px',
+              border: '1px solid #49475B',
+            }}
           >
             <h3>{user.role === 'seeker' ? 'Applied' : 'Very Interested'}</h3>
             {saved
